fc2d45dee81c0f7f4c2e1d64c775d341424586c2
Zach Becker <doc.misterio@gmail.com>
2020-10-27 14:37:34 -0500

    somehwere between the project working and me logging on today to add more code I changed the self in the nav bar button item to nil and that hosed everything

--- project4/ViewController.swift
+++ project4/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: nil, action: #selector(openTapped))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
