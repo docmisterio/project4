f093722e1e34c03f28c4e7e68a3c62005fb60279
Zach Becker <doc.misterio@gmail.com>
2020-10-29 14:52:16 -0500

    back and forth linting seeing how I feel about how it looks. bug still exists. I hate this fucking bug

--- project4/ViewController.swift
+++ project4/ViewController.swift
@@ -94,13 +94,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
                 }
             }
         }
-        let alert = UIAlertController(
-            title: "Not Allowed",
-            message: "Browsing outside of this website is not allowed",
-            preferredStyle: .alert)
-        alert.addAction(UIAlertAction(
-            title: "Ok, I won't do it again.",
-            style: .cancel))
+        let alert = UIAlertController(title: "Not Allowed", message: "Browsing outside of this website is not allowed", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok, I won't do it again.", style: .cancel))
         
         decisionHandler(.cancel)
         present(alert, animated: true)
