4088199dc5d14be0cf201fecf638b18733a1b958
Zach Becker <doc.misterio@gmail.com>
2020-10-30 16:12:01 -0500

    Project 4: Challenge 3 - view controller created for the table view and got the list from the array to show up, nee to hook it up to the webview ViewController

---
+++ project4/WebsiteTableView.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class WebsiteTableView: UITableViewController {
+    var websites: [String]! = []
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        websites = ["google.com", "hackingwithswift.com"]
+        return websites.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = websites[indexPath.row]
+        return cell
+    }
+    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        if let vc = storyboard?.instantiateViewController(identifier: "webView") {
+//            navigationController?.present(vc, animated: true)
+//        }
+//    }
+}
